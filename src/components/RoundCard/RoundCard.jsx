3ba3b77096df0c0294df3ea65bdae10e36b0d81b
KaelorFogo <kfogo19@gmail.com>
2024-02-27 22:31:41 -0700

    Course name renders from API data

--- src/components/RoundCard/RoundCard.jsx
+++ src/components/RoundCard/RoundCard.jsx
@@ -1,14 +1,13 @@
-import { useEffect, useState } from "react";
 import "./RoundCard.css";
 
 export default function RoundCard({ round }) {
+  const date = new Date(round.date).toLocaleDateString();
+
   return (
     <div className="card">
       <div className="card-content">
-        <h2 className="course-name">Course: {round.course.name}</h2>{" "}
-        <p className="player-score">
-          Date: {new Date(round.date).toLocaleDateString()}
-        </p>
+        <h2 className="course-name">Course: {round.course[0].name}</h2>
+        <p className="player-score">Date: {date}</p>
       </div>
     </div>
   );
