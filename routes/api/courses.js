6c29a7f031af24a934c29dc6736abfb1d98cbefd
KaelorFogo <kfogo19@gmail.com>
2024-03-05 17:46:56 -0700

    ensure logged in

--- routes/api/courses.js
+++ routes/api/courses.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const coursesCtrl = require('../../controllers/api/courses');
 
-router.get('/:name', coursesCtrl.getCourse);
+router.get('/:name', ensureLoggedIn, coursesCtrl.getCourse);
 
 module.exports = router;
--- routes/api/rounds.js
+++ routes/api/rounds.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const roundsCtrl = require('../../controllers/api/rounds');
+const ensureLoggedIn = require('../../config/ensureLoggedIn');
+
 
 // POST /api/rounds (create a user - sign up)
-router.post('/', roundsCtrl.create);
+router.post('/', ensureLoggedIn, roundsCtrl.create);
 
-router.get('/', roundsCtrl.getAllRoundsForuser);
+router.get('/', ensureLoggedIn, roundsCtrl.getAllRoundsForuser);
 
-router.get('/:id', roundsCtrl.show);
+router.get('/:id', ensureLoggedIn, roundsCtrl.show);
 
-router.delete('/:id', roundsCtrl.deleteById);
+router.delete('/:id', ensureLoggedIn, roundsCtrl.deleteById);
 
 module.exports = router;
