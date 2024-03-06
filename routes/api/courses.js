03de206dfd45e401b991c588f27f80ec44c9c7c7
KaelorFogo <kfogo19@gmail.com>
2024-03-05 17:47:37 -0700

    ensure logged in courses

--- routes/api/courses.js
+++ routes/api/courses.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const coursesCtrl = require('../../controllers/api/courses');
+const ensureLoggedIn = require('../../config/ensureLoggedIn');
 
 router.get('/:name', ensureLoggedIn, coursesCtrl.getCourse);
 
