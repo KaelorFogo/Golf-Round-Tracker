1443c4dddd463ffa3cc493e744b80be497b72a29
KaelorFogo <kfogo19@gmail.com>
2024-07-06 23:02:46 -0600

    nav bar recoloring

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -2,18 +2,21 @@ import "./LandingPage.css";
 
 export default function LandingPage({ user }) {
   return (
-    user ? (
+    <>
+    <img className="logo" src="https://i.imgur.com/QlddchK.png" title="source: imgur.com" />
+    {user ? (
       <div className="landing-page">
-        <h1 className="landing-page-title">Welcome to Your Golf App, {user}!</h1>
+        <h1 className="landing-page-title">Welcome to Circle2, {user}!</h1>
         <p className="landing-page-text">Here, you can track your golf rounds, analyze your performance, and improve your game.</p>
         <p className="landing-page-text">Start by adding a new round or explore your past rounds to see how you've progressed.</p>
       </div>
     ) : (
       <div className="landing-page">
-        <h1 className="landing-page-title">Welcome to Birdie Tracker!</h1>
+        <h1 className="landing-page-title">Welcome to Circle2!</h1>
         <p className="landing-page-text">Here, you can track your golf rounds, analyze your performance, and improve your game.</p>
         <p className="landing-page-text">Start by logging in or signing up.</p>
       </div>
-    )
+    )}
+  </>
   );
 }
